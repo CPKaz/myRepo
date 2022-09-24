716ebc8368cad47d43f27ed167d7fed526f9a735
CPKaz <ck7aj@virginia.edu>
2022-09-24 11:39:13 -0700

    first commit!

---
+++ myFile.js
@@ -0,0 +1,9 @@
+function myFunction(a, b) {
+    if ( a > b) {
+        return "a is greater that b.";
+    } else if (b < a) {
+        return "b is greater than a.";
+    } else {
+        return "Not sure what to do.";
+    }
+}
