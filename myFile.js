266f74d36812ca65ce33c03e7002fb855668a15b
CPKaz <ck7aj@virginia.edu>
2022-09-27 21:17:03 -0700

    deleted files

--- myFile.js
+++
@@ -1,9 +0,0 @@
-function myFunction(a, b) {
-    if ( a > b) {
-        return "a is greater that b.";
-    } else if (b < a) {
-        return "b is greater than a.";
-    } else {
-        return "Not sure what to do.";
-    }
-}
