589de5ebb28b859a6c2337717274a84a4b6a2fee
CPKaz <ck7aj@virginia.edu>
2022-09-27 21:53:48 -0700

    bool interpretter

--- myFile.js
+++ myFile.js
@@ -1,13 +1,22 @@
 function myFunction(myString) {
     if (myString == "marco") {
-        return "polo"
+        return "polo";
     } else if (myString == "peanut butter") {
-        return "jelly"
+        return "jelly";
     } else if (myString == "king") {
-        return "of queens"
+        return "of queens";
     }
 }
 
 function counter(myInt) {
     return myInt + 1;
+}
+
+function boolInterpretter(myBool) {
+    if (myBool) {
+        return true;
+    }
+    else if (!myBool) {
+        return false;
+    }
 }
